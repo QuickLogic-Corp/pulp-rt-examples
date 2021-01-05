3ba909409a01073a9ff2c79dafdf59e731294640
timsaxe <saxe@quicklogic.com>
2021-01-05 13:12:16 -0800

    added clock speed overrides and I2C set for CCS811

--- hello/test.c
+++ hello/test.c
@@ -15,6 +15,10 @@
  */
 
 #include <stdio.h>
+#include <rt/rt_api.h>
+
+int __rt_fpga_fc_frequency = 		10000000; // e.g. 20000000 for 20MHz;
+int __rt_fpga_periph_frequency = 	 5000000; // e.g. 10000000 for 10MHz;
 
 int main()
 {
--- periph/i2c/test.c
+++ periph/i2c/test.c
@@ -2,6 +2,9 @@
 #include <rt/rt_api.h>
 #include <stdint.h>
 
+int __rt_fpga_fc_frequency = 		10000000; // e.g. 20000000 for 20MHz;
+int __rt_fpga_periph_frequency =	 5000000; // e.g. 10000000 for 10MHz;
+
 #define BUFFER_SIZE 3
 
 RT_L2_DATA uint8_t tx_buffer[BUFFER_SIZE];
@@ -10,7 +13,7 @@ RT_L2_DATA uint8_t rx_buffer[BUFFER_SIZE];
 
 int main()
 {
-  printf("Entering test\n");
+  printf("Entering test\r\n");
 
   rt_i2c_conf_t conf;
 
@@ -18,7 +21,7 @@ int main()
 
   // This will tell the driver to use the i2c 0 interface (in case there are several)
   conf.id = 0;
-  conf.cs = 0xAA;
+  conf.cs = 0x5B;	// CCS811
   conf.max_baudrate = 100000;
 
   rt_i2c_t *i2c = rt_i2c_open(NULL, &conf, NULL);
@@ -31,16 +34,19 @@ int main()
 
   // No event is specified so we are doing a blocking call, which means
   // we are blocked on this call until the whole buffer is sent
-  rt_i2c_write(i2c, tx_buffer, BUFFER_SIZE, 0, NULL);
+  // rt_i2c_write(i2c, tx_buffer, BUFFER_SIZE, 0, NULL);
+  rt_i2c_write(i2c, 0x20, 1, 0, NULL);		// Read reg 0x20 (HW_ID)
 
-  //rt_i2c_read(i2c, rx_buffer, BUFFER_SIZE, 0, NULL);
+  rt_i2c_read(i2c, rx_buffer, 1, 0, NULL);	// Retrieve HW_ID
+  
+  printf("HW_ID: 0x%02x\r\n", rx_buffer[0]);
 
   //rt_i2c_control(i2c, RT_I2C_CTRL_SET_MAX_BAUDRATE, 2000000);
 
-  volatile int i;
-  for (i=0; i<10000; i++);
+  // volatile int i;
+  // for (i=0; i<10000; i++);
   
-  rt_i2c_write(i2c, tx_buffer, BUFFER_SIZE, 0, NULL);
+  // rt_i2c_write(i2c, tx_buffer, BUFFER_SIZE, 0, NULL);
 
   return 0;
 }
